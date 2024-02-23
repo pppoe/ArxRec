8daf68652508f6e4d0cb9fa6e6c73c2db06f4138
Haoxiang Li <haoxiang.li@localhost>
2024-02-23 20:31:05 +0000

    update <> added util to get gs token

---
+++ gs_auth.py
@@ -0,0 +1,15 @@
+import argparse
+import json
+import gspread
+
+if __name__ == '__main__':
+
+    args = argparse.ArgumentParser()
+    args.add_argument('--google_oauth', type=str, default=None, required=True, help='Path to Google OAuth Credentials json file')
+    args.add_argument('--google_oauth_user', type=str, default=None, required=False, help='Path to Google OAuth Authorized User json file')
+    args = args.parse_args()
+
+    google_oauth_user = None
+    gc, authorized_user = gspread.oauth_from_dict(json.load(open(args.google_oauth)), google_oauth_user)
+    with open(args.google_oauth_user,'w') as fp:
+        fp.write(authorized_user)
